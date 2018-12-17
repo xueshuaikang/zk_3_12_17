c2de8be254ee088d22c4ddd94ef671857fc6c53f
xuenuo <626096325@qq.com>
2018-12-17 10:13:16 +0800

    接口开发完成

--- routes/users.js
+++ routes/users.js
@@ -2,8 +2,19 @@ var express = require('express');
 var router = express.Router();
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+router.post('/conmon', function(req, res, next) {
+  let {year}=req.body,
+      {month}=req.body,
+      {day}=req.body;
+    console.log(year)
+    console.log(month)
+    console.log(day)
+    
+  if(!year && !month && !day){
+    res.json({code:0});
+  }else{
+    res.json({code:9});
+  }
 });
 
 module.exports = router;
